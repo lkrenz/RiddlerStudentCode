6f17b7563fc4ed93a74bf14f5fafe3baff592a69
lkrenz <liamkrenz100@gmail.com>
2024-08-22 19:19:33 -0700

    Started working on the code for passages 1-3.

--- WORK_LOG.md
+++ WORK_LOG.md
@@ -1,9 +1,10 @@
 Name: YOUR NAME HERE
 
-| Date   |    Time    |                                                                    Update |
-|:-------|:----------:|--------------------------------------------------------------------------:|
-| Aug 22 |  8-8:30pm  | I started by thinking about my place in the universe.<br/>It was scary... |
-|        |            |                                                             And then I... |
+| Date   |        Time         |                                                                                                                                                                                                                           Update |
+|:-------|:-------------------:|---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------:|
+| Aug 22 | 30 Minutes in class |                                                                                                                                                       I spent this time figuring out the encryption for the first three passages |
+| Aug 22 |      6:30-7:00      | Started coding the methods for part one and two. I didn't quite finish because I kept having to look up java syntax (It's been a while). Also, I can't tell if the functions will run because I'm running into an issue with JDK |
+| Aug 22 |      7:00-7:15      |                                                                                             Finished writing code for the second and third passages. I will keep working on this tomorrow once I figure out how to run the code. |
 
 
 To add a new row to the table, click into a cell and then hit shift-enter.
--- src/Riddler.java
+++ src/Riddler.java
@@ -1,3 +1,6 @@
+import java.sql.Array;
+import java.util.ArrayList;
+
 /**
  * The Riddler:
  * A puzzle by Zach Blick
@@ -11,7 +14,24 @@ public class Riddler {
     public String decryptOne(String encrypted) {
         String decrypted = "";
 
-        // TODO: Complete the decryptOne() function.
+        // Convert the inputted string into an array of chars to use enhanced for loop
+        char[] encryptedArray = encrypted.toCharArray();
+
+        // Use the askii values of each character to shift the value by 9
+        for (char i : encryptedArray) {
+            char newText = i;
+            // Use two separate cases for uppercase and lowercase
+            if (i <= 90 && i >= 65) {
+                newText += 9;
+                if (newText > 90)
+                    newText -= 26;
+            } else if (i >= 97 && i <= 122) {
+                newText += 9;
+                if (newText > 122)
+                    newText -= 26;
+            }
+            decrypted += newText;
+        }
 
         return decrypted;
     }
@@ -20,7 +40,12 @@ public class Riddler {
         String decrypted = "";
 
         // TODO: Complete the decryptTwo() function.
-
+        String[] newText = encrypted.split(" ", 0);
+        for (String i : newText) {
+            int integerValue = Integer.parseInt(i);
+            char letter = (char) integerValue;
+            decrypted += letter;
+        }
         return decrypted;
     }
 
@@ -28,6 +53,12 @@ public class Riddler {
         String decrypted = "";
 
         // TODO: Complete the decryptThree() function.
+        for (int i = 0; i + 8 < encrypted.length(); i += 8) {
+            String numbers = encrypted.substring(i, i+8);
+            int decimal = Integer.parseInt(numbers, 2);
+            decrypted += (char) decimal;
+        }
+
 
         return decrypted;
     }
